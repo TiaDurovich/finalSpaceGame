9a3915d9eb3b4bb1abe9c0b295f7b2154efd4926
Tia Durovich <64508223+TiaDurovich@users.noreply.github.com>
2021-10-01 15:43:22 +1300

    Update checkCollision.js
    Comments

--- js/checkCollision.js
+++ js/checkCollision.js
@@ -1,16 +1,19 @@
+// This function checks collisions in every frame between missile and enemy, enemy and place, and plane and coins
+// The scoreboard is then changed accordingly
+
 function checkCollision(thePlane, theCoins, theEnemies, theMissiles, score, health) {
 
     if (thePlane.model) {
 
         planeBB = new THREE.Box3().setFromObject(thePlane.model);
 
-        // HANDLING COLLISION 
+        // Handling collisions by determining intersection between two models using their positions and dimensions
         var i = theCoins.length;
         while (i--) {
             if (theCoins[i]) {
                 coinBB = new THREE.Box3().setFromObject(theCoins[i].model);
                 if (planeBB.intersectsBox(coinBB)) {
-                    score += 1;
+                    score += 1; // When the plane collides with a coin score is increased by 1
                     theCoins[i].destroy();
                     theCoins.splice(i, 1);
                     document.getElementById("scoreboard").innerHTML = "HEALTH: " + health + " &emsp; COINS: " + score; 
@@ -24,8 +27,8 @@ function checkCollision(thePlane, theCoins, theEnemies, theMissiles, score, heal
                 enemyBB = new THREE.Box3().setFromObject(theEnemies[i].model);
 
                 if (planeBB.intersectsBox(enemyBB)) {
-                    health -= 1;
-                    theEnemies[i].destroy();
+                    health -= 1; // When the plane collides with an enemy health is decrease by 1
+                    theEnemies[i].destroy(); //Enemy is destroyed (removed from scene)
                     theEnemies.splice(i, 1);
                     document.getElementById("scoreboard").innerHTML = "HEALTH: " + health + " &emsp; COINS: " + score; 
                 }
@@ -36,10 +39,10 @@ function checkCollision(thePlane, theCoins, theEnemies, theMissiles, score, heal
                 if (theMissiles[j]) {
                     missileBB = new THREE.Box3().setFromObject(theMissiles[j].model);
                     if (missileBB.intersectsBox(enemyBB)) {
-                        score += 2;
-                        theEnemies[i].destroy();
+                        score += 2; // When a missile collides with an enemy score increase by 2
+                        theEnemies[i].destroy(); // Enemy is destroyed (removed from scene)
                         theEnemies.splice(i, 1);
-                        theMissiles[j].destroy();
+                        theMissiles[j].destroy(); // Missile is also destroyed (removed from scene)
                         theMissiles.splice(j, 1);
                         document.getElementById("scoreboard").innerHTML = "HEALTH: " + health + " &emsp; COINS: " + score; 
                     } 
